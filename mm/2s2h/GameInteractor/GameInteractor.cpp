6715ec66ff99bc13932bd3137a8f8a709a9576f5
lightmanLP <liteman1000@gmail.com>
2025-02-22 13:31:02 +0700

    update settings

--- mm/2s2h/GameInteractor/GameInteractor.cpp
+++ mm/2s2h/GameInteractor/GameInteractor.cpp
@@ -294,16 +294,6 @@ int GameInteractor_InvertControl(GIInvertType type) {
                 result *= -1;
             }
             break;
-        case GI_INVERT_CAMERA_MOUSE_X:
-            if (CVarGetInteger("gEnhancements.Camera.Mouse.InvertX", 0)) {
-                result *= -1;
-            }
-            break;
-        case GI_INVERT_CAMERA_MOUSE_Y:
-            if (CVarGetInteger("gEnhancements.Camera.Mouse.InvertY", 1)) {
-                result *= -1;
-            }
-            break;
         case GI_INVERT_FIRST_PERSON_AIM_X:
             if (CVarGetInteger("gEnhancements.Camera.FirstPerson.InvertX", 0)) {
                 result *= -1;
@@ -334,16 +324,6 @@ int GameInteractor_InvertControl(GIInvertType type) {
                 result *= -1;
             }
             break;
-        case GI_INVERT_FIRST_PERSON_MOUSE_X:
-            if (CVarGetInteger("gEnhancements.Camera.Mouse.FirstPerson.InvertX", 0)) {
-                result *= -1;
-            }
-            break;
-        case GI_INVERT_FIRST_PERSON_MOUSE_Y:
-            if (CVarGetInteger("gEnhancements.Camera.Mouse.FirstPerson.InvertY", 1)) {
-                result *= -1;
-            }
-            break;
         case GI_INVERT_SHIELD_X:
             if (CVarGetInteger("gEnhancements.Shield.InvertX", 0)) {
                 result *= -1;
@@ -360,7 +340,6 @@ int GameInteractor_InvertControl(GIInvertType type) {
     if (CVarGetInteger("gModes.MirroredWorld.State", 0)) {
         switch (type) {
             case GI_INVERT_CAMERA_RIGHT_STICK_X:
-            case GI_INVERT_CAMERA_MOUSE_X:
             case GI_INVERT_MOVEMENT_X:
             case GI_INVERT_SHIELD_X:
             case GI_INVERT_SHOP_X:
@@ -371,7 +350,6 @@ int GameInteractor_InvertControl(GIInvertType type) {
             case GI_INVERT_FIRST_PERSON_AIM_X:
             case GI_INVERT_FIRST_PERSON_GYRO_X:
             case GI_INVERT_FIRST_PERSON_RIGHT_STICK_X:
-            case GI_INVERT_FIRST_PERSON_MOUSE_X:
             case GI_INVERT_FIRST_PERSON_MOVING_X:
                 result *= -1;
                 break;
